9032a3ac365b23b3247e82af66a27c9cc6774e43
Vijay Kumar <chunchuvijaykumar96@gmail.com>
2021-11-03 20:13:56 -0500

    updated results in Rabbit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/Rabbit', RabbitRouter)
+app.use('/Rabbit', RabbitRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
