7a1e05336b845a3f123db79afdf1f55168a8d98c
Vijay Kumar <chunchuvijaykumar96@gmail.com>
2021-11-03 19:28:54 -0500

    updated file

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var RabbitRouter = require('./routes/Rabbit');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/Rabbit', RabbitRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
